23fa3c71f20d469fd9556ebddd03989441ee8965
AnabolKiev <anabol.ua@gmail.com>
2019-03-06 01:25:17 +0200

    Per anus approach

---
+++ src/db/create_tables.sql
@@ -0,0 +1,67 @@
+DROP SCHEMA IF EXISTS movieland;
+CREATE SCHEMA movieland;
+
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS movieGenre;
+DROP TABLE IF EXISTS movieCountry;
+DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS country;
+
+
+CREATE TABLE user (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(30) UNIQUE NOT NULL,
+  nickname VARCHAR(30),
+  password VARCHAR(76),
+  salt VARCHAR(76),
+  role VARCHAR(20)
+);
+
+CREATE TABLE genre (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) UNIQUE
+);
+
+CREATE TABLE country (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) UNIQUE
+);
+
+CREATE TABLE movie (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nameRussian VARCHAR(100) ,
+  nameNative VARCHAR(100),
+  description VARCHAR(1000),
+  yearOfRelease VARCHAR(4),
+  rating DOUBLE,
+  price DOUBLE,
+  picturePath VARCHAR(255)
+);
+
+CREATE TABLE movieGenre (
+  movieId INT,
+  genreId INT,
+  
+  FOREIGN KEY (movieId) REFERENCES movie(id) ON DELETE CASCADE,
+  FOREIGN KEY (genreId) REFERENCES genre(id) ON DELETE CASCADE
+);
+
+CREATE TABLE movieCountry (
+  movieId INT,
+  countryId INT,
+  
+  FOREIGN KEY (movieId) REFERENCES movie(id) ON DELETE CASCADE,
+  FOREIGN KEY (countryId) REFERENCES country(id) ON DELETE CASCADE
+);
+
+CREATE TABLE review (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  movieId INT,
+  userId INT,
+  text VARCHAR(1000),
+  
+  FOREIGN KEY (movieId) REFERENCES movie(id) ON DELETE CASCADE,
+  FOREIGN KEY (userId) REFERENCES user(id) ON DELETE CASCADE  
+);
---
+++ src/main/java/Main.java
@@ -0,0 +1,204 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import static java.sql.DriverManager.getConnection;
+
+public class Main {
+    private static final String URL_DB = "jdbc:mysql://localhost:3306/movieland";
+    private static final String USERNAME_DB = "root";
+    private static final String PASSWORD_DB = "root";
+    private static Connection connection;
+    private static Statement statement;
+    private static PreparedStatement preparedStatement;
+
+    public static void main(String[] args) throws IOException, SQLException {
+        List<String> users = readFromFile("src/main/resources/user.txt");
+        List<String> genres = readFromFile("src/main/resources/genre.txt");
+        List<String> movies = readFromFile("src/main/resources/movie.txt");
+        List<String> reviews = readFromFile("src/main/resources/review.txt");
+        List<String> posters = readFromFile("src/main/resources/poster.txt");
+
+        connection = getConnection(URL_DB, USERNAME_DB, PASSWORD_DB);
+        statement = connection.createStatement();
+
+        insertUser(users);
+        insertGenre(genres);
+        insertMovies(movies);
+        insertReviews(reviews);
+        updateMovieWithPoster(posters);
+
+        statement.close();
+        connection.close();
+    }
+
+    private static List<String> readFromFile(String path) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        ArrayList<String> lines = new ArrayList<>();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            if (line.length() != 0) {
+                if (line.startsWith("\uFEFF")) {
+                    line = line.substring(1);
+                }
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
+
+    private static void insertUser(List<String> users) throws SQLException {
+        String name, email, password;
+        for (int i = 0; i < users.size() / 3; i++) {
+            name = users.get(i * 3);
+            email = users.get(i * 3 + 1);
+            password = users.get(i * 3 + 2);
+            String insertQuery = "INSERT INTO user(email, nickname, password)" +
+                    " VALUES ('" + email + "', '" + name + "', '" + password + "');";
+            //System.out.println(insertQuery);
+            statement.executeUpdate(insertQuery);
+        }
+    }
+
+    private static void insertGenre(List<String> genres) throws SQLException {
+        String name;
+        for (int i = 0; i < genres.size(); i++) {
+            name = genres.get(i);
+            String insertQuery = "INSERT INTO genre(name) VALUES ('" + name + "');";
+            //System.out.println(insertQuery);
+            statement.executeUpdate(insertQuery);
+        }
+    }
+
+    private static void insertMovies(List<String> movies) throws SQLException {
+        String nameRussian, nameNative, yearOfRelease, countries, genres, description;
+        double rating, price;
+        int movieId = 0;
+
+        HashSet<String> countrySet = new HashSet<>();
+        for (int i = 0; i < movies.size() / 7; i++) {
+            countries = movies.get(i * 7 + 2);
+            String[] countryArray = countries.split(", ");
+            for (String countryName : countryArray) {
+                countrySet.add(countryName);
+            }
+        }
+        insertCountry(countrySet);
+
+        for (int i = 0; i < movies.size() / 7; i++) {
+            String[] names = movies.get(i * 7).split("/");
+            nameRussian = names[0];
+            nameNative = names[1];
+            yearOfRelease = movies.get(i * 7 + 1);
+            countries = movies.get(i * 7 + 2);
+            genres = movies.get(i * 7 + 3);
+            description = movies.get(i * 7 + 4);
+            rating = new Double(movies.get(i * 7 + 5).substring(7));
+            price = new Double(movies.get(i * 7 + 6).substring(6));
+
+            String insertQuery = "INSERT INTO movie(nameRussian, nameNative, yearOfRelease, description, rating, price)" +
+                    " VALUES (?, ?, ?, ?, ?, ?);";
+            //System.out.println(insertQuery);
+            //System.out.println(countries);
+            //System.out.println(genres);
+
+            //statement.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            //ResultSet resultSet = statement.getGeneratedKeys();
+
+            preparedStatement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, nameRussian);
+            preparedStatement.setString(2, nameNative);
+            preparedStatement.setString(3, yearOfRelease);
+            preparedStatement.setString(4, description);
+            preparedStatement.setDouble(5, rating);
+            preparedStatement.setDouble(6, price);
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                movieId = resultSet.getInt(1);
+                insertMovieCountry(movieId, countries);
+                insertMovieGenre(movieId, genres);
+            }
+        }
+
+
+    }
+
+    private static void insertCountry(HashSet<String> countrySet) throws SQLException {
+        for (String countryName : countrySet) {
+            String insertQuery = "INSERT INTO country(name) VALUES ('" + countryName + "');";
+            //System.out.println(insertQuery);
+            statement.executeUpdate(insertQuery);
+        }
+    }
+
+    private static void insertMovieCountry(int movieId, String countries) throws SQLException {
+        String[] countryArray = countries.split(", ");
+        for (String countryName : countryArray) {
+            ResultSet resultSet = statement.executeQuery("SELECT id FROM country WHERE name = '" + countryName + "';");
+            if (resultSet.next()) {
+                int countryId = resultSet.getInt(1);
+                String insertQuery = "INSERT INTO movieCountry(movieId, countryId)" +
+                        " VALUES ('" + movieId + "', '" + countryId + "');";
+                //System.out.println(insertQuery);
+                statement.executeUpdate(insertQuery);
+            }
+        }
+    }
+
+    private static void insertMovieGenre(int movieId, String genres) throws SQLException {
+        String[] genreArray = genres.split(", ");
+        for (String genreName : genreArray) {
+            ResultSet resultSet = statement.executeQuery("SELECT id FROM genre WHERE name = '" + genreName + "';");
+            if (resultSet.next()) {
+                int genreId = resultSet.getInt(1);
+                String insertQuery = "INSERT INTO movieGenre(movieId, genreId)" +
+                        " VALUES ('" + movieId + "', '" + genreId + "');";
+                statement.executeUpdate(insertQuery);
+            }
+        }
+    }
+
+    private static void updateMovieWithPoster(List<String> posters) throws SQLException {
+        for (String line : posters) {
+            int index = line.indexOf("https");
+            String movieName = line.substring(0, index - 1);
+            String url = line.substring(index);
+            String updateQuery = "UPDATE movie SET picturePath = '" + url + "' WHERE nameRussian = '" + movieName + "';";
+            statement.executeUpdate(updateQuery);
+        }
+    }
+
+    private static void insertReviews(List<String> reviews) throws SQLException {
+        for (int i = 0; i < reviews.size() / 3; i++) {
+            String movieName = reviews.get(i * 3);
+            String nickName = reviews.get(i * 3 + 1);
+            String description = reviews.get(i * 3 + 2);
+
+            int movieId = 0;
+            int userId = 0;
+
+            String sql = "SELECT id FROM movie WHERE nameRussian = ?;";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, movieName);
+            preparedStatement.executeQuery();
+            ResultSet resultSet = preparedStatement.getResultSet();
+            if (resultSet.next()) {
+                movieId = resultSet.getInt(1);
+            }
+            resultSet = statement.executeQuery("SELECT id FROM user WHERE nickname = '" + nickName + "';");
+            if (resultSet.next()) {
+                userId = resultSet.getInt(1);
+            }
+
+            String insertQuery = "INSERT INTO review(movieId, userId, text)" +
+                    " VALUES ('" + movieId + "', '" + userId + "', '" + description + "');";
+            System.out.println(insertQuery);
+            statement.executeUpdate(insertQuery);
+        }
+    }
+}
